18ca20e88c95d84b7a44e2f220701b09917b7165
Naman Agrawal <na77@njit.edu>
2021-02-09 23:58:32 +0000

    Calculate winner of game or tie and display on status

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,16 +2,39 @@ import React, { useState, useCallback } from 'react';
 
 import Cell from './Cell';
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 function Board() {
   const [board, setBoard] = useState(Array(9).fill(null));
-  const [currentTurn, setCurrentTurn] = useState(1);
+  const [currentTurn, setCurrentTurn] = useState(1); // 1 to 9
 
   const currentCellValue = currentTurn % 2 === 0 ? 'O' : 'X';
 
-  const isGameComplete = board.some(cell => cell == null);
+  const isBoardFull = board.every(cell => cell != null);
+
+  const winner = calculateWinner(board);
+  const gameHasWinner = winner != null;
 
   const clickCell = useCallback((index) => {
-    if (board[index] == null) {
+    if (board[index] == null && !gameHasWinner && !isBoardFull) {
       const boardCopy = board.slice();
       boardCopy[index] = currentCellValue;
       setBoard(boardCopy);
@@ -21,9 +44,11 @@ function Board() {
 
   return (
     <div className="board">
-      {isGameComplete ?
-        <h2>Next Player: {currentCellValue}</h2> : 
-        <h2>Game complete!</h2>
+      {gameHasWinner ?
+        <h2>Game complete - Winner is {winner}!</h2>
+       : isBoardFull ?
+            <h2>Game complete - Tie</h2> :
+            <h2>Next Player: {currentCellValue}</h2>
       }
       {board.map((value, i) => {
         return <>
